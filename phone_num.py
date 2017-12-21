7c299ca3d53146f0ccfd677fb8bb94f73bdd19ef
yeml <1205287882@qq.com>
2017-12-22 00:37:27 +0800

    Hello word

---
+++ phone_num.py
@@ -0,0 +1,12 @@
+import string
+import random
+first=1
+f=open("d:/1.txt",'w')
+num = int(input('请输入要生成电话号码的条数是：'))
+for i in range(num):
+    start = str(first) + ''.join(random.sample(string.digits, 2))
+    end = ''.join(random.sample(string.digits, 8))
+    phonenum = start + end
+    f.writelines(phonenum+'\n')
+    print('电话号码是：'+phonenum)
+f.close()
